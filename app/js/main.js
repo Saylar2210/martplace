1efeaa19c14e75f85e4e83f1c728ea6d7d376de0
Saylar2210 <bochorov@mail.ru>
2020-09-30 19:55:52 +0300

    added new-items

--- app/js/main.js
+++ app/js/main.js
@@ -14,19 +14,30 @@ $(function () {
 
     });
 
+
+
+  $('.weekly__slider').slick({
+   
+    arrow: true,
+    prevArrow: '<button class="slick-arrow slick-prev__weekly"></button>',
+    nextArrow: '<button class="slick-arrow slick-next__weekly"></button>'
+
+
+
+
+
+  });
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+});
+$(".rate-star").rateYo({
+  rating: 5,
+  starWidth: "12px",
+  readOnly: true
 });
 
 
-
-// $('.cart').slick({
-
-// });
-
-// $('.block-star__star').rateYo({
-//     starWidth:'13px',
-//     rating:'4.5',
-//     ratedFill: '#ffc000',
-//     readOnly:true,
-//     normalFill:'transparent',
-//     starSvg: '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path fill="#ffc000" d="M12 7.13l.97 2.29.47 1.11 1.2.1 2.47.21-1.88 1.63-.91.79.27 1.18.56 2.41-2.12-1.28-1.03-.64-1.03.62-2.12 1.28.56-2.41.27-1.18-.91-.79-1.88-1.63 2.47-.21 1.2-.1.47-1.11.97-2.27M12 2L9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2z"/></svg>'
-// });
